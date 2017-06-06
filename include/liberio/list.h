1838652cfc3e902500006468bc1f758277419854
Alex Williams <alex.williams@ni.com>
2017-06-06 10:31:53 -0700

    Fixed bad casts that required -fpermissive.

--- include/liberio/list.h
+++ include/liberio/list.h
@@ -133,8 +133,8 @@ static inline void __list_del_entry(struct list_head *entry)
 static inline void list_del(struct list_head *entry)
 {
 	__list_del(entry->prev, entry->next);
-	entry->next = LIST_POISON1;
-	entry->prev = LIST_POISON2;
+	entry->next = (struct list_head *) LIST_POISON1;
+	entry->prev = (struct list_head *) LIST_POISON2;
 }
 #else
 extern void __list_del_entry(struct list_head *entry);
@@ -624,8 +624,8 @@ static inline void __hlist_del(struct hlist_node *n)
 static inline void hlist_del(struct hlist_node *n)
 {
 	__hlist_del(n);
-	n->next = LIST_POISON1;
-	n->pprev = LIST_POISON2;
+	n->next = (struct hlist_node *) LIST_POISON1;
+	n->pprev = (struct hlist_node **) LIST_POISON2;
 }
 
 static inline void hlist_del_init(struct hlist_node *n)
