bba00c1be7ebd13f9fcfe523fde7be4596de077c
Moritz Fischer <moritz.fischer@ettus.com>
2017-07-27 09:58:15 -0700

    util: Add liberio-util file with some udev helpers
    This adds a bunch of helper functions to deal with udev stuff,
    as well as functions to get sysattrs from liberio_channels.
    
    Signed-off-by: Moritz Fischer <moritz.fischer@ettus.com>

---
+++ src/liberio-util.c
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2017, National Instruments Corp.
+ *
+ * "That dude over there just punched Merica into that guy,
+ *  he must be a Liberio!"
+ * 	- Urban Dictionary
+ *
+ * SPDX-License-Identifier: GPL-2.0
+ *
+ */
+
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <stdint.h>
+#include <libudev.h>
+
+#include "priv.h"
+
+struct udev_device *liberio_udev_device_from_fd(struct udev *udev,
+						int fd)
+{
+	struct udev_device *dev;
+	struct stat statbuf;
+	int err;
+
+	err = fstat(fd, &statbuf);
+	if (err < 0)
+		return NULL;
+
+	if (!S_ISCHR(statbuf.st_mode))
+		return NULL;
+
+	return udev_device_new_from_devnum(udev, 'c', statbuf.st_rdev);
+}
+
+const char *liberio_chan_get_sysattr(struct liberio_chan *chan,
+				     const char *sysattr)
+{
+	if (!chan || !sysattr)
+		return NULL;
+
+	return udev_device_get_sysattr_value(chan->dev, sysattr);
+}
+
+int liberio_chan_set_sysattr(struct liberio_chan *chan, const char *sysattr,
+			     char *value)
+{
+	if (!chan || !sysattr || !value)
+		return -EINVAL;
+
+	return udev_device_set_sysattr_value(chan->dev, sysattr, value);
+}
--- src/util.h
+++ src/util.h
@@ -1,7 +1,24 @@
-#ifndef UTIL_H
-#define UTIL_H
+/*
+ * Copyright (c) 2017, National Instruments Corp.
+ *
+ * "That dude over there just punched Merica into that guy,
+ *  he must be a Liberio!"
+ * 	- Urban Dictionary
+ *
+ * SPDX-License-Identifier: GPL-2.0
+ *
+ */
+
+#ifndef LIBERIO_UTIL_H
+#define LIBERIO_UTIL_H
 
 #define likely(x)       __builtin_expect((x),1)
 #define unlikely(x)     __builtin_expect((x),0)
 
-#endif /* UTIL_H */
+struct udev_device;
+struct udev;
+
+struct udev_device *liberio_udev_device_from_fd(struct udev *udev,
+						int fd);
+
+#endif /* LIBERIO_UTIL_H */
